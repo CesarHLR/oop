1eeb5bcb6235b6c883752fe3e6f849473078695f
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-01 12:53:57 -0700

    Juego

---
+++ JuegoDeLaVida/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JuegoDeLaVida
+{
+    enum Estado{viva, muerta}
+
+    class Celula
+    {
+        public  Estado estado_actual;
+        public  Estado estado_siguiente;
+        public Tablero tablero;
+        public short renglon;
+        public short columna;
+        
+
+        public Celula(Estado inicial,Tablero tablero, short renglon, short columna)
+        {
+            estado_actual=inicial;
+            this.renglon=renglon;
+            this.columna=columna;
+
+        }
+    }
+    class Tablero
+    {
+         private List<List<Celula>> grid;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("▒");
+        }
+    }
+}
