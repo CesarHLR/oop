e3413d32d3bcbe4aa9d681e33a4128cf698abf25
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-03 12:54:04 -0700

    prueba 3

--- JuegoDeLaVida/Program.cs
+++ JuegoDeLaVida/Program.cs
@@ -20,40 +20,71 @@ namespace JuegoDeLaVida
             this.tablero=tablero;
             this.renglon=renglon;
             this.columna=columna;
+        }
+        public void actualiza_estado()
+        {
 
         }
+        public short num_vecinas()
+        {
+            short cuenta=0;
+            //Posicion 1 
+            if(renglon>0 && columna>0)
+            {
+                if(tablero.grid[renglon-1][columna-1].estado_actual==Estado.viva)
+                {
+                    cuenta++;
+                }
+            }
+            return cuenta;
+        }
+        
         public void print()
         {
             if(this.estado_actual==Estado.vacia)
             {
-            Console.Write("o");
+            Console.Write("▒");
             }
             else
             {
-            Console.Write("x");
+            Console.Write("▓");
             }
         }
     }
     class Tablero
     {
-         private List<List<Celula>> grid;
-         public Tablero()
+         public List<List<Celula>> grid;
+         public Tablero(short num_renglon, short num_columna)
          {
            grid=new List<List<Celula>>();
+           for(short i=0; i<=num_renglon-1; i++)
+           {
+               grid.Add(new List<Celula>());
+               for(short j=0; j<=num_columna-1; j++)
+               {
+                   grid[i].Add(new Celula(Estado.vacia,this,i,j));
+               }
+           }
+           /* 
            grid.Add(new List<Celula>());
-           grid[0].Add(new Celula(Estado.viva,this,0,0));
-           grid[0].Add(new Celula(Estado.vacia,this,0,1));
-           grid[0].Add(new Celula(Estado.vacia,this,0,2));
+           grid[i].Add(new Celula(Estado.viva,this,i,0));
+           grid[i].Add(new Celula(Estado.vacia,this,i,1));
+           grid[i].Add(new Celula(Estado.vacia,this,i,2));
 
            grid.Add(new List<Celula>());
-           grid[0].Add(new Celula(Estado.vacia,this,1,0));
-           grid[0].Add(new Celula(Estado.vacia,this,1,1));
-           grid[0].Add(new Celula(Estado.viva,this,1,2));
+           grid[1].Add(new Celula(Estado.vacia,this,1,0));
+           grid[1].Add(new Celula(Estado.vacia,this,1,1));
+           grid[1].Add(new Celula(Estado.viva,this,1,2));
 
            grid.Add(new List<Celula>());
-           grid[0].Add(new Celula(Estado.vacia,this,2,0));
-           grid[0].Add(new Celula(Estado.vacia,this,2,1));
-           grid[0].Add(new Celula(Estado.viva,this,2,2));
+           grid[2].Add(new Celula(Estado.vacia,this,2,0));
+           grid[2].Add(new Celula(Estado.vacia,this,2,1));
+           grid[2].Add(new Celula(Estado.viva,this,2,2));
+           */
+         }
+         public void inserta(Celula c)
+         {
+             grid[c.renglon][c.columna]=c;
          }
          public void print()
          {
@@ -64,6 +95,7 @@ namespace JuegoDeLaVida
                     c.print();
                     
                 }
+                Console.WriteLine();
              }
          }
     }
@@ -75,8 +107,14 @@ namespace JuegoDeLaVida
             Console.WriteLine("▒");
             Console.WriteLine("▒");
          */
-         Tablero Gol=new Tablero();
+         Tablero Gol=new Tablero(10,5);
+         Gol.inserta(new Celula(Estado.viva, Gol, 3,3 ));
+         Gol.inserta(new Celula(Estado.viva, Gol, 3,2 ));
+         Gol.inserta(new Celula(Estado.viva, Gol, 3,1 ));
+         Gol.inserta(new Celula(Estado.viva, Gol, 0,0 ));
+         
          Gol.print();
+         Console.WriteLine(Gol.grid [1],[1].num_vecinas());
         }
     }
-}
+  }
